20670af77fbc8670ab5fe59da69f798fc5b4978d
LeChenMusic Dev <dev@lechenmusic.com>
2026-07-11 11:51:42 +0800

    fix: 添加缺失的TextAlign import

--- app/src/main/java/com/lechenmusic/ui/screens/audiobook/AudiobookScreen.kt
+++ app/src/main/java/com/lechenmusic/ui/screens/audiobook/AudiobookScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
