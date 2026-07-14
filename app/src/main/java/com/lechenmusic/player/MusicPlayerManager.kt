8e2c2387403e3f54dd993850c3d148610f9d45d3
LeChenMusic Dev <dev@lechenmusic.com>
2026-07-14 22:38:58 +0800

    fix: 锁屏15秒按钮直接使用PNG资源ID

--- app/src/main/java/com/lechenmusic/player/MusicPlayerManager.kt
+++ app/src/main/java/com/lechenmusic/player/MusicPlayerManager.kt
@@ -464,21 +464,11 @@ class MusicPlayerManager(private val context: Context) {
                     context, 6, forwardIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
                 )
-                // Scale skip icons to match other notification button size (24dp)
-                val iconSize = (24 * context.resources.displayMetrics.density).toInt()
-                val skipBackBmp = Bitmap.createScaledBitmap(
-                    BitmapFactory.decodeResource(context.resources, R.drawable.ic_skip_backward_15),
-                    iconSize, iconSize, true)
-                val skipFwdBmp = Bitmap.createScaledBitmap(
-                    BitmapFactory.decodeResource(context.resources, R.drawable.ic_skip_forward_15),
-                    iconSize, iconSize, true)
-                val skipBackIconCompat = androidx.core.graphics.drawable.IconCompat.createWithBitmap(skipBackBmp)
-                val skipFwdIconCompat = androidx.core.graphics.drawable.IconCompat.createWithBitmap(skipFwdBmp)
                 builder
                     .addAction(NotificationCompat.Action(R.drawable.ic_notif_prev, "上一章", prevPending))
-                    .addAction(NotificationCompat.Action(skipBackIconCompat.toIcon(context), "后退15秒", rewindPending))
+                    .addAction(NotificationCompat.Action(R.drawable.ic_skip_backward_15, "后退15秒", rewindPending))
                     .addAction(NotificationCompat.Action(playPauseIcon, if (_isPlaying.value) "暂停" else "播放", playPausePending))
-                    .addAction(NotificationCompat.Action(skipFwdIconCompat.toIcon(context), "前进15秒", forwardPending))
+                    .addAction(NotificationCompat.Action(R.drawable.ic_skip_forward_15, "前进15秒", forwardPending))
                     .addAction(NotificationCompat.Action(R.drawable.ic_notif_next, "下一章", nextPending))
                 builder.setStyle(
                     MediaStyle()
