c6fe82f3f1c0cd5bb66db0c09870aa389c7bc68b
yueyoue <165250587+yueyoue@users.noreply.github.com>
2026-07-23 13:06:33 +0800

    fix: 修复编译错误

--- app/src/main/java/com/lechenmusic/ui/screens/favorites/TabletFavoritesScreen.kt
+++ app/src/main/java/com/lechenmusic/ui/screens/favorites/TabletFavoritesScreen.kt
@@ -1,4 +1,5 @@
 package com.lechenmusic.ui.screens.favorites
+import androidx.compose.foundation.background
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
